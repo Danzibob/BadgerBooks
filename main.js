87c9f3cf772904bc69eba7fec011df80ff4cdc4c
Danzibob <Danzibob@gmail.com>
2016-10-26 10:57:04 +0100

    Increased mobile support, polishing

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function smoothScroll(elt){
-	console.log($(elt).offset().top-40);
-	$('html, body').animate({scrollTop: $(elt).offset().top-40}, 500);
+	var pos = $(elt).offset().top + $(elt).height()/2 - $(window).height()/2
+	console.log(pos);
+	$('html, body').animate({scrollTop: pos}, 500);
 }
 
 function fadeSwap(div, to){
@@ -25,12 +26,32 @@ function slideshow(div){
 // Interval of the slideshow in ms. 8000 (8s) default
 var intervalID = setInterval(slideshow, 2000,ss_div)
 
+var page_width = $("#tile0 .wrapper").children().first().outerHeight()
 function fwd(tile){
     var wrap = $(tile+" .wrapper")
-    var page = -parseInt(wrap.css("margin-left").slice(0,-2))/600
+	page_width = wrap.children().first().outerHeight()
+    var page = -parseInt(wrap.css("margin-left").slice(0,-2))/page_width
     if(page >= wrap.children(".pane").length - 1){
         wrap.animate({marginLeft: '0'}, 500);
     } else {
-        wrap.animate({marginLeft: '-=600'}, 500);
+        wrap.animate({marginLeft: '-=' + page_width.toString()}, 500);
     }
 }
+var resizeTimer
+$(window).resize(function(){
+	// clearTimeout(resizeTimer);
+	resizeTimer = setTimeout(function() {
+		console.log("ran")
+		$(".wrapper").each(function(){
+			var new_width = $(this).children().first().outerHeight()
+			console.log(new_width, page_width)
+			var page = -parseInt($(this).css("margin-left").slice(0,-2))/page_width
+			if(page <= $(this).children().length){
+				var pos = -page*new_width
+			} else {
+				var pos = 0
+			}
+			$(this).css("margin-left",String(pos)+'px')
+		})
+	}, 250);
+})
